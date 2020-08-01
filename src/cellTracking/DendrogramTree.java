ac0a4baa46f33e7c1c7b02b0ec65107e902314e2
Raghavendrapara <raghavendra0360@gmail.com>
2020-08-01 13:42:12 +0530

    Attempt at starting dendrogramTree and Viterbi generic

---
+++ src/cellTracking/DendrogramTree.java
@@ -0,0 +1,47 @@
+package cellTracking;
+
+import javax.swing.ImageIcon;
+
+public class DendrogramTree {
+// Root node pointer. Will be null for an empty tree.
+private TreeNode root;
+
+
+private static class TreeNode {
+ TreeNode left;
+ TreeNode right;
+ ImageIcon icon;
+
+ TreeNode(ImageIcon newdata) {
+   left = null;
+   right = null;
+   icon = newdata;
+ }
+}
+
+public void DendrogramTree() {
+ root = null;
+}
+
+
+
+public void insert(ImageIcon data,String pos) {
+ root = insert(root, data, pos);
+}
+
+private TreeNode insert(TreeNode node, ImageIcon imgicon,String pos) {
+ if (node==null) {
+   node = new TreeNode(imgicon);
+ }
+ else {
+   if (pos==null) {
+     node.left = insert(node.left, imgicon, pos);
+   }
+   else {
+     node.right = insert(node.right, imgicon, pos);
+   }
+ }
+
+ return(node); // in any case, return the new pointer to the caller
+}
+}
